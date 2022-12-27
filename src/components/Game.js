6b292d5e2ec6082f3d38a6ca87e781a83eb1ae41
Noah Segal <n.h.segal@gmail.com>
2022-12-27 12:10:49 +0900

    Fix name coordinate association

--- src/components/Game.js
+++ src/components/Game.js
@@ -18,23 +18,23 @@ const facecoodinates = [
   "1400,173,40",
   // Seated
   "154,287,40",
-  "204,333,40",
+     "204,333,40",
   "297,294,40",
-  "367,338,40",
+     "367,338,40",
   "448,288,40",
-  "527,333,40",
+     "527,333,40",
   "616,288,40",
-  "702,336,40",
+     "702,336,40",
   "796,268,40",
-  "892,326,40",
+    "892,326,40",
   "936,268,40",
-  "1042,326,40",
+      "1042,326,40",
   "1086,278,40",
-  "1162,326,40",
+    "1162,326,40",
   "1246,278,40",
-  "1332,326,40",
+    "1332,326,40",
   "1390,290,40",
-  "1450,340,40",
+     "1450,340,40",
 ];
 
 const names = [
@@ -114,76 +114,79 @@ const photoKey = [
   coordinates:"1400,173,40",
   name: "Léon Brillouin",
   },
+
+
+
   {
   coordinates:"154,287,40",    
   name:  "Peter Debye",
   },
   {
-  coordinates:"204,333,40",
+  coordinates:  "297,294,40",
   name: "Martin Knudsen",
   },
   {    
-  coordinates:"297,294,40",
+  coordinates: "448,288,40",
   name: "William Lawrence Bragg",
   },
   {
-  coordinates:"367,338,40",
+  coordinates:"616,288,40",
   name: "Hendrik Anthony Kramers",
   },
   {
-  coordinates:"448,288,40",    
+  coordinates:  "796,268,40",    
   name: "Paul Dirac",
   },
   {
-  coordinates:"527,333,40",
+  coordinates:  "936,268,40",
   name: "Arthur Compton",
   },
   { 
-  coordinates:"616,288,40",
+  coordinates:  "1086,278,40",
   name: "Louis de Broglie",
   },
   { 
-  coordinates:"702,336,40",
+  coordinates:"1246,278,40",
   name: "Max Born",
   },
   {  
-  coordinates:"796,268,40",
+  coordinates:"1390,290,40",
   name: "Niels Bohr",
   },
   {
-  coordinates:"892,326,40",
+  coordinates: "204,333,40",
   name: "Irving Langmuir",
   },
   {  
-  coordinates:"936,268,40",
+  coordinates: "367,338,40",
   name: "Max Planck",
   },
   {
-  coordinates:"1042,326,40",
+  coordinates:"527,333,40",
   name:  "Marie Curie",
   },
   {
-  coordinates: "1086,278,40",
+  coordinates:  "702,336,40",
   name: "Hendrik Lorentz",
   },
   {
-  coordinates:"1162,326,40",
+  coordinates: "892,326,40",
   name:  "Albert Einstein",
   },
   {
-  coordinates:"1246,278,40",
+  coordinates:  "1042,326,40",
   name:  "Paul Langevin",
   },
   {
-  coordinates:"1332,326,40",
+  coordinates:  "1162,326,40",
   name:  "Charles-Eugène Guye",
   },
   {
-  coordinates:"1390,290,40",
+  coordinates: "1332,326,40",
   name: "Charles Thomson Rees Wilson",
   },
   {
-  coordinates:"1450,340,40",
+  coordinates:  "1450,340,40",
   name: "Owen Willans Richardson"
   }
 ]
@@ -197,44 +200,24 @@ function Game() {
   return (
     <div className={"container"}>
        {
-       facecoodinates.map(function (entry, index) {
-        let x = entry.split(',')[0]
-        let y = entry.split(',')[1]
+       photoKey.map(function (entry, index) {
+        let x = entry.coordinates.split(',')[0]
+        let y = entry.coordinates.split(',')[1]
 
         const myComponentStyle = {
           top: `${y}px`,
-          left: `${x}px`,
+          left: `${x-50}px`,
          }
           return (
-            <div className={"tag-div"} key={entry + 100} style={myComponentStyle}  >
-            Where is this label
+            <div className={"tag-div"} key={entry.coordinates + 100} style={myComponentStyle}  >
+            {`${entry.name}`}
           </div>
           )})}
 
 
 
       <img src={photo} alt="Solvay Conference" useMap="#testmap" />
-{
-      /*
-      {facecoodinates.map(function (entry, index) {
-        let x = entry.split(',')[0]
-        let y = entry.split(',')[1]
 
-        const myComponentStyle = {
-          color: 'blue',
-          position: 'absolute',
-          top: `${x}`,
-          left: `${y}`,
-         }
-
-        }
-          return (
-            <div className={"tag-div"} key={entry + 100} style={myComponentStyle}  >
-            Where is this label
-          </div>
-          )})}
-     */
-          }
       <map name="testmap">
         {facecoodinates.map(function (entry, index) {
           return (
