8bb14dbf8995895f3b4a4527360b7fe0dca18545
Noah Segal <n.h.segal@gmail.com>
2022-12-27 11:57:04 +0900

    Create list of coords and names joined

--- src/components/Game.js
+++ src/components/Game.js
@@ -69,7 +69,124 @@ const names = [
   "Owen Willans Richardson",
 ];
 
-
+const photoKey = [
+  {
+  coordinates: "325,145,40",
+  name:  "Auguste Piccard",
+  },
+  {
+  coordinates: "438,173,40",
+  name: "Émile Henriot",
+  },
+  {
+  coordinates: "520,208,40",
+  name: "Paul Ehrenfest",
+  },
+  {
+  coordinates: "654,192,40",
+  name: "Édouard Herzen",
+  },
+  { 
+  coordinates: "790,208,40",
+  name: "Théophile de Donder",
+  },
+  {
+  coordinates:"914,180,40",
+  name : "Erwin Schrödinger",
+  },
+  {
+  coordinates: "1024,180,40",
+  name: "Jules-Émile Verschaffelt",
+  },
+  { 
+  coordinates: "1138,180,40",
+  name: "Wolfgang Pauli",
+  },
+  {
+  coordinates: "1260,170,40",
+  name: "Werner Heisenberg",
+  },
+  {
+  coordinates:"1338,130,40",
+  name: "Ralph Howard Fowler",
+  },
+  {
+  coordinates:"1400,173,40",
+  name: "Léon Brillouin",
+  },
+  {
+  coordinates:"154,287,40",    
+  name:  "Peter Debye",
+  },
+  {
+  coordinates:"204,333,40",
+  name: "Martin Knudsen",
+  },
+  {    
+  coordinates:"297,294,40",
+  name: "William Lawrence Bragg",
+  },
+  {
+  coordinates:"367,338,40",
+  name: "Hendrik Anthony Kramers",
+  },
+  {
+  coordinates:"448,288,40",    
+  name: "Paul Dirac",
+  },
+  {
+  coordinates:"527,333,40",
+  name: "Arthur Compton",
+  },
+  { 
+  coordinates:"616,288,40",
+  name: "Louis de Broglie",
+  },
+  { 
+  coordinates:"702,336,40",
+  name: "Max Born",
+  },
+  {  
+  coordinates:"796,268,40",
+  name: "Niels Bohr",
+  },
+  {
+  coordinates:"892,326,40",
+  name: "Irving Langmuir",
+  },
+  {  
+  coordinates:"936,268,40",
+  name: "Max Planck",
+  },
+  {
+  coordinates:"1042,326,40",
+  name:  "Marie Curie",
+  },
+  {
+  coordinates: "1086,278,40",
+  name: "Hendrik Lorentz",
+  },
+  {
+  coordinates:"1162,326,40",
+  name:  "Albert Einstein",
+  },
+  {
+  coordinates:"1246,278,40",
+  name:  "Paul Langevin",
+  },
+  {
+  coordinates:"1332,326,40",
+  name:  "Charles-Eugène Guye",
+  },
+  {
+  coordinates:"1390,290,40",
+  name: "Charles Thomson Rees Wilson",
+  },
+  {
+  coordinates:"1450,340,40",
+  name: "Owen Willans Richardson"
+  }
+]
 
 
 function Game() {
@@ -79,7 +196,6 @@ function Game() {
 
   return (
     <div className={"container"}>
-       <div className="test-label">HELLO!</div>
        {
        facecoodinates.map(function (entry, index) {
         let x = entry.split(',')[0]
